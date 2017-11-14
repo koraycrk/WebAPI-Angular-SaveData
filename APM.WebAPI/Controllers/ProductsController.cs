2d53ddc585aeab32a014d0eb211bc4a31d07cf70
koraycrk <30293677+koraycrk@users.noreply.github.com>
2017-11-14 21:13:45 +0300

    Create Project
    Create Project

---
+++ APM.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,65 @@
+﻿using APM.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.OData;
+
+namespace APM.WebAPI.Controllers
+{
+    [EnableCorsAttribute("http://localhost:52436", "*", "*")]
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        [EnableQuery()]
+        public IQueryable<Product> Get()
+        {
+            var productRepository = new ProductRepository();
+            return productRepository.Retrieve().AsQueryable();
+        }
+      
+        // GET: api/Products/5
+        public Product Get(int id)
+        {
+            Product product;
+            var productRepository = new ProductRepository();
+
+            if (id > 0)
+            {
+                var products = productRepository.Retrieve();
+                product = products.FirstOrDefault(p => p.ProductId == id);
+            }
+            else
+            {
+                product = productRepository.Create();
+            }
+            return product;
+        }
+
+
+
+
+
+        // POST: api/Products
+        public void Post([FromBody]Product product)
+        {
+            var productRepository = new Models.ProductRepository();
+            var newProduct = productRepository.Save(product);
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]Product product)
+        {
+            var productRepository = new Models.ProductRepository();
+            var updatedProduct = productRepository.Save(id, product);
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
